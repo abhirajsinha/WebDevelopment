12426885c28e42f296eb6cfeddf3d5f6cefb1a8f
Abhiraj Sinha <abhirajsinha@Abhirajs-MacBook-Air.local>
2022-06-02 23:34:08 +0530

    add

--- WebScrapping/index.js
+++ WebScrapping/index.js
@@ -1,24 +1,7 @@
-// let fs = require('fs');
-// let data = fs.readFileSync("f1.txt");
+let fs = require('fs');
+let data = fs.readFileSync("f1.txt");
 
 
-// console.log(" "+data);
+console.log(" "+data);
 
-// // Updated Nodec
-
-const http = require('http');
-const port = 8000;
-
-
-const server = http.createServer();
-
-
-
-server.listen(port, function(err){
-    if(err){
-        console.log(err);
-        return;
-    }
-
-    console.log("Server is up and running on port:", port);
-});
+// Updated Nodec
