cb6d8373b3d7cf34fe1fa28c5f937ce475b08ac4
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-06-04 14:35:49 +0530

    Chalk

--- WebScrapping/corona.js
+++ WebScrapping/corona.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio');
 const request = require("request");
+const chalk = require('chalk');
+
 request('https://www.worldometers.info/coronavirus/',cb);
 
 console.log("Before");
@@ -17,6 +19,13 @@ function cb(error, response, html) {
 
   function handleHtml(html){
      let setTool =  cheerio.load(html);
-     let h1 = setTool("h1");
-     console.log(h1.length);
+     let h1 = setTool("#maincounter-wrap span");
+
+     let total = setTool(h1[0]).text();
+     let deaths = setTool(h1[1]).text();
+     let rec = setTool(h1[2]).text();
+
+     console.log(chalk.gray("Total Cases: "+total));
+     console.log(chalk.red("Deaths: "+deaths));
+     console.log(chalk.yellow("Recovered: "+rec));
   }
